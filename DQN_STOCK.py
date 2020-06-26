c9078c8fc9770035533edfb10240d23088f1ed7d
forrestneo <zhenshida@foxmail.com>
2020-06-26 14:12:02 +0800

    Add files via upload

---
+++ DQN_STOCK.py
@@ -0,0 +1,439 @@
+import os
+import numpy as np
+import sys
+import codecs
+
+import paddle.fluid as fluid
+import parl
+from parl import layers
+from parl.utils import logger
+import random
+import json
+import gym
+from gym import spaces
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+LEARN_FREQ = 5 # 训练频率，不需要每一个step都learn，攒一些新增经验后再learn，提高效率
+MEMORY_SIZE = 20000    # replay memory的大小，越大越占用内存
+MEMORY_WARMUP_SIZE = 200  # replay_memory 里需要预存一些经验数据，再从里面sample一个batch的经验让agent去learn
+BATCH_SIZE = 32   # 每次给agent learn的数据数量，从replay memory随机里sample一批数据出来
+GAMMA = 0.99 # reward 的衰减因子，一般取 0.9 到 0.999 不等
+######################################################################
+######################################################################
+#
+# 1. 请设定 learning rate，可以从 0.001 起调，尝试增减
+#
+######################################################################
+######################################################################
+LEARNING_RATE = 4e-1 # 学习率
+
+class Model(parl.Model):
+    def __init__(self, act_dim):
+        ######################################################################
+        ######################################################################
+        #
+        # 2. 请参考课堂Demo，配置model
+        #
+        hid1_size = 128
+        hid2_size = 128
+        # 3层全连接网络
+        self.fc1 = layers.fc(size=hid1_size, act='relu')
+        self.fc2 = layers.fc(size=hid2_size, act='relu')
+        self.fc3 = layers.fc(size=act_dim, act=None)
+        ######################################################################
+        ######################################################################
+
+    def value(self, obs):
+        # 定义网络
+        # 输入state，输出所有action对应的Q，[Q(s,a1), Q(s,a2), Q(s,a3)...]
+        
+        ######################################################################
+        ######################################################################
+        #
+        # 3. 请参考课堂Demo，组装Q网络
+        h1 = self.fc1(obs)
+        h2 = self.fc2(h1)
+        Q = self.fc3(h2)
+        #
+        ######################################################################
+        ######################################################################
+        return Q
+
+
+from parl.algorithms import DQN
+
+class Agent(parl.Agent):
+    def __init__(self,
+                 algorithm,
+                 obs_dim,
+                 act_dim,
+                 e_greed=0.1,
+                 e_greed_decrement=0):
+        assert isinstance(obs_dim, int)
+        assert isinstance(act_dim, int)
+        self.obs_dim = obs_dim
+        self.act_dim = act_dim
+        super(Agent, self).__init__(algorithm)
+
+        self.global_step = 0
+        self.update_target_steps = 200  # 每隔200个training steps再把model的参数复制到target_model中
+
+        self.e_greed = e_greed  # 有一定概率随机选取动作，探索
+        self.e_greed_decrement = e_greed_decrement  # 随着训练逐步收敛，探索的程度慢慢降低
+
+    def build_program(self):
+        self.pred_program = fluid.Program()
+        self.learn_program = fluid.Program()
+
+        with fluid.program_guard(self.pred_program):  # 搭建计算图用于 预测动作，定义输入输出变量
+            obs = layers.data(
+                name='obs', shape=[self.obs_dim], dtype='float32')
+            self.value = self.alg.predict(obs)
+
+        with fluid.program_guard(self.learn_program):  # 搭建计算图用于 更新Q网络，定义输入输出变量
+            obs = layers.data(
+                name='obs', shape=[self.obs_dim], dtype='float32')
+            action = layers.data(name='act', shape=[1], dtype='int32')
+            reward = layers.data(name='reward', shape=[], dtype='float32')
+            next_obs = layers.data(
+                name='next_obs', shape=[self.obs_dim], dtype='float32')
+            terminal = layers.data(name='terminal', shape=[], dtype='bool')
+            self.cost = self.alg.learn(obs, action, reward, next_obs, terminal)
+
+    def sample(self, obs):
+        sample = np.random.rand()  # 产生0~1之间的小数
+        if sample < self.e_greed:
+            act = np.random.randint(self.act_dim)  # 探索：每个动作都有概率被选择
+        else:
+            act = self.predict(obs)  # 选择最优动作
+        self.e_greed = max(
+            0.01, self.e_greed - self.e_greed_decrement)  # 随着训练逐步收敛，探索的程度慢慢降低
+        return act
+
+    def predict(self, obs):  # 选择最优动作
+        obs = np.expand_dims(obs, axis=0)
+        pred_Q = self.fluid_executor.run(
+            self.pred_program,
+            feed={'obs': obs.astype('float32')},
+            fetch_list=[self.value])[0]
+        pred_Q = np.squeeze(pred_Q, axis=0)
+        act = np.argmax(pred_Q)  # 选择Q最大的下标，即对应的动作
+        return act
+
+    def learn(self, obs, act, reward, next_obs, terminal):
+        # 每隔200个training steps同步一次model和target_model的参数
+        if self.global_step % self.update_target_steps == 0:
+            self.alg.sync_target()
+        self.global_step += 1
+
+        act = np.expand_dims(act, 1)
+        feed = {
+            'obs': obs.astype('float32'),
+            'act': act.astype('int32'),
+            'reward': reward,
+            'next_obs': next_obs.astype('float32'),
+            'terminal': terminal
+        }
+        cost = self.fluid_executor.run(
+            self.learn_program, feed=feed, fetch_list=[self.cost])[0]  # 训练一次网络
+        return cost
+import math
+
+import numpy as np
+
+import gym
+from gym import spaces
+from gym.utils import seeding
+
+MAX_ACCOUNT_BALANCE = 214748
+MAX_NUM_SHARES = 214748
+MAX_SHARE_PRICE = 5000
+MAX_VOLUME = 1000e6
+MAX_AMOUNT = 3e5
+MAX_OPEN_POSITIONS = 5
+MAX_STEPS = 500
+MAX_DAY_CHANGE = 1
+max_loss =-50000
+
+INITIAL_ACCOUNT_BALANCE = 10000
+
+
+class StockTradingEnv(gym.Env):
+    """A stock trading environment for OpenAI gym"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, df):
+        super(StockTradingEnv, self).__init__()
+
+        self.df = df
+        self.reward_range = (0, MAX_ACCOUNT_BALANCE)
+
+        # Actions of the format Buy x%, Sell x%, Hold, etc.
+        self.action_space = spaces.Box(
+            low=0, high=1,shape=(3,), dtype=np.float32)
+
+        # Prices contains the OHCL values for the last five prices
+        self.observation_space = spaces.Box(
+            low=0, high=1, shape=(19,), dtype=np.float32)
+
+    def _next_observation(self):
+        obs = np.array([
+            self.df.loc[self.current_step, 'open'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'high'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'low'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'close'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'volume'] / MAX_VOLUME,
+            self.df.loc[self.current_step, 'amount'] / MAX_AMOUNT,
+            self.df.loc[self.current_step, 'adjustflag'] / 10,
+            self.df.loc[self.current_step, 'tradestatus'] / 1,
+            self.df.loc[self.current_step, 'pctChg'] / 100,
+            self.df.loc[self.current_step, 'peTTM'] / 1e4,
+            self.df.loc[self.current_step, 'pbMRQ'] / 100,
+            self.df.loc[self.current_step, 'psTTM'] / 100,
+            self.df.loc[self.current_step, 'pctChg'] / 1e3,
+            self.balance / MAX_ACCOUNT_BALANCE,
+            self.max_net_worth / MAX_ACCOUNT_BALANCE,
+            self.shares_held / MAX_NUM_SHARES,
+            self.cost_basis / MAX_SHARE_PRICE,
+            self.total_shares_sold / MAX_NUM_SHARES,
+            self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE),
+        ])
+        return obs
+
+    def _take_action(self, action):
+        # Set the current price to a random price within the time step
+        current_price = random.uniform(
+            self.df.loc[self.current_step, "open"], self.df.loc[self.current_step, "close"])
+        action_type = action
+        amount = action
+
+        if action_type < 1:
+            # Buy amount % of balance in shares
+            total_possible = int(self.balance / current_price)
+            shares_bought = int(total_possible * amount)
+            prev_cost = self.cost_basis * self.shares_held
+            additional_cost = shares_bought * current_price
+
+            self.balance -= additional_cost
+            self.cost_basis = (
+                prev_cost + additional_cost) / (self.shares_held + shares_bought)
+            self.shares_held += shares_bought
+
+        elif action_type < 2:
+            # Sell amount % of shares held
+            shares_sold = int(self.shares_held * amount)
+            self.balance += shares_sold * current_price
+            self.shares_held -= shares_sold
+            self.total_shares_sold += shares_sold
+            self.total_sales_value += shares_sold * current_price
+
+        self.net_worth = self.balance + self.shares_held * current_price
+
+        if self.net_worth > self.max_net_worth:
+            self.max_net_worth = self.net_worth
+
+        if self.shares_held == 0:
+            self.cost_basis = 0
+
+    def step(self, action):
+        # Execute one time step within the environment
+        self._take_action(action)
+        done = False
+
+        self.current_step += 1
+
+        if self.current_step > len(self.df.loc[:, 'open'].values) - 1:
+            self.current_step = 0  # loop training
+            done = True
+
+        delay_modifier = (self.current_step / MAX_STEPS)
+
+        # profits
+        reward = self.net_worth - INITIAL_ACCOUNT_BALANCE
+        reward = 1 if reward > 0 else -100
+
+        if self.net_worth <= 0 :
+            done = True
+        if reward <= max_loss :
+            done = True
+
+        obs = self._next_observation()
+
+        return obs, reward, done, {}
+
+    def reset(self, new_df=None):
+        # Reset the state of the environment to an initial state
+        self.balance = INITIAL_ACCOUNT_BALANCE
+        self.net_worth = INITIAL_ACCOUNT_BALANCE
+        self.max_net_worth = INITIAL_ACCOUNT_BALANCE
+        self.shares_held = 0
+        self.cost_basis = 0
+        self.total_shares_sold = 0
+        self.total_sales_value = 0
+
+        # pass test dataset to environment
+        if new_df:
+            self.df = new_df
+
+        # Set the current step to a random point within the data frame
+        # self.current_step = random.randint(
+        #     0, len(self.df.loc[:, 'open'].values) - 6)
+        self.current_step = 0
+
+        return self._next_observation()
+
+    def render(self, mode='human'):
+        # Render the environment to the screen
+        profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
+        print('-'*30)
+        print(f'Step: {self.current_step}')
+        print(f'Balance: {self.balance}')
+        print(f'Shares held: {self.shares_held} (Total sold: {self.total_shares_sold})')
+        print(f'Avg cost for held shares: {self.cost_basis} (Total sales value: {self.total_sales_value})')
+        print(f'Net worth: {self.net_worth} (Max net worth: {self.max_net_worth})')
+        print(f'Profit: {profit}')
+        return profit
+
+# replay_memory.py
+import random
+import collections
+import numpy as np
+
+
+class ReplayMemory(object):
+    def __init__(self, max_size):
+        self.buffer = collections.deque(maxlen=max_size)
+
+    # 增加一条经验到经验池中
+    def append(self, exp):
+        self.buffer.append(exp)
+
+    # 从经验池中选取N条经验出来
+    def sample(self, batch_size):
+        mini_batch = random.sample(self.buffer, batch_size)
+        obs_batch, action_batch, reward_batch, next_obs_batch, done_batch = [], [], [], [], []
+
+        for experience in mini_batch:
+            s, a, r, s_p, done = experience
+            obs_batch.append(s)
+            action_batch.append(a)
+            reward_batch.append(r)
+            next_obs_batch.append(s_p)
+            done_batch.append(done)
+
+        return np.array(obs_batch).astype('float32'), \
+            np.array(action_batch).astype('float32'), np.array(reward_batch).astype('float32'),\
+            np.array(next_obs_batch).astype('float32'), np.array(done_batch).astype('float32')
+
+    def __len__(self):
+        return len(self.buffer)
+# 训练一个episode
+def run_episode(env, agent, rpm):
+    total_reward = 0
+    obs = env.reset()
+    step = 0
+    while True:
+        step += 1
+        action = agent.sample(obs)  # 采样动作，所有动作都有概率被尝试到
+        
+        next_obs, reward, done, _ = env.step(action)
+        rpm.append((obs, action, reward, next_obs, done))
+
+        # train model
+        if (len(rpm) > MEMORY_WARMUP_SIZE) and (step % LEARN_FREQ == 0):
+            (batch_obs, batch_action, batch_reward, batch_next_obs,
+             batch_done) = rpm.sample(BATCH_SIZE)
+            train_loss = agent.learn(batch_obs, batch_action, batch_reward,
+                                     batch_next_obs,
+                                     batch_done)  # s,a,r,s',done
+
+        total_reward += reward
+        obs = next_obs
+        if done :
+            break
+        #print(step,total_reward)
+    return total_reward
+
+
+# 评估 agent, 跑 5 个episode，总reward求平均
+def evaluate(env, agent, render=False):
+    eval_reward = []
+    for i in range(5):
+        obs = env.reset()
+        episode_reward = 0
+        while True:
+            action = agent.predict(obs)  # 预测动作，只选最优动作
+            obs, reward, done, _ = env.step(action)
+            episode_reward += reward
+            if render:
+                env.render()
+            if done:
+                break
+        eval_reward.append(episode_reward)
+    return np.mean(eval_reward)
+
+font = fm.FontProperties(fname='font/wqy-microhei.ttc')
+# plt.rc('font', family='Source Han Sans CN')
+plt.rcParams['axes.unicode_minus'] = False
+
+
+# 创建环境
+#env = gym.make('Pong-v0')
+df = pd.read_csv('sh.600055.csv')
+df = df.sort_values('date')
+
+# The algorithms require a vectorized environment to run
+env = StockTradingEnv(df)
+
+# 创建环境
+
+action_dim = env.action_space.shape[0]
+obs_shape = env.observation_space.shape[0] 
+# 创建经验池
+rpm = ReplayMemory(MEMORY_SIZE)  # DQN的经验回放池
+
+
+
+# 根据parl框架构建agent
+######################################################################
+######################################################################
+#
+# 4. 请参考课堂Demo，嵌套Model, DQN, Agent构建 agent
+#
+######################################################################
+######################################################################
+model = Model(act_dim=action_dim)
+algorithm = DQN(model, act_dim=action_dim, gamma=GAMMA, lr=LEARNING_RATE)
+agent = Agent(
+    algorithm,
+    obs_dim=obs_shape,
+    act_dim=action_dim,
+    e_greed=0.1,  # 有一定概率随机选取动作，探索
+    e_greed_decrement=1e-6)  # 随着训练逐步收敛，探索的程度慢慢降低
+
+# 加载模型
+# save_path = './dqn_model.ckpt'
+# agent.restore(save_path)
+
+# 先往经验池里存一些数据，避免最开始训练的时候样本丰富度不够
+while len(rpm) < MEMORY_WARMUP_SIZE:
+    run_episode(env, agent, rpm)
+
+max_episode = 500
+
+# 开始训练
+episode = 0
+while episode < max_episode:  # 训练max_episode个回合，test部分不计算入episode数量
+    # train part
+    for i in range(0, 1):
+        total_reward = run_episode(env, agent, rpm)
+        episode += 1
+    # test part
+    eval_reward = evaluate(env, agent, render=False)  # render=True 查看显示效果
+    logger.info('episode:{}    e_greed:{}   test_reward:{}'.format(
+        episode, agent.e_greed, eval_reward))
+# 训练结束，保存模型
+save_path = './dqn_model.ckpt'
+agent.save(save_path)
